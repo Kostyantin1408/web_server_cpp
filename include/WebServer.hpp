8260b2588f2fcd392b2bcf6b014bfe76da49c8a3
Oleksandr Ivaniuk <oleksandr.ivaniuk@roboneers.net>
2025-04-02 18:03:52 +0300

    Added multithreading with stop

--- include/WebServer.hpp
+++ include/WebServer.hpp
@@ -1,41 +1,50 @@
 #ifndef WEBSOCKET___WEBSERVER_HPP
 #define WEBSOCKET___WEBSERVER_HPP
 
-#include <utility>
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
-#include <iostream>
-#include <functional>
 #include "HttpRequest.hpp"
-
+#include <functional>
+#include <netinet/in.h>
+#include <stop_token>
+#include <thread>
 
 class WebServer {
 public:
-    WebServer();
+  struct Parameters {
+    std::string host{};
+    int port{};
+  };
 
-    void listen(int port);
+  WebServer(Parameters parameters_);
 
-    void start_accept();
+  void listen(int port);
 
-    void run();
+  void start_accept();
 
-    void post(const std::string &route, std::function<void(int, HttpRequest)> handler);
+  void run();
 
-    void get(const std::string &route, std::function<void(int, HttpRequest)> handler);
+  void post(const std::string &route, std::function<void(int, HttpRequest)> handler);
 
+  void get(const std::string &route, std::function<void(int, HttpRequest)> handler);
 
-    static int static_server_fd;
+  void worker(std::stop_token);
+
+  void wait_for_exit();
+
+  static int static_server_fd;
 
 private:
-    int server_fd;
-    sockaddr_in address;
-    volatile bool stop_server;
+  Parameters parameters;
+  int server_fd;
+  sockaddr_in address;
+  volatile bool stop_server;
+  std::jthread server_thread;
+  std::stop_source stop_source;
 
-    std::unordered_map<std::string, std::function<void(int, HttpRequest)>> get_handlers;
-    std::unordered_map<std::string, std::function<void(int, HttpRequest)>> post_handlers;
-    void on_http(int client_fd);
+  bool is_running = false;
 
+  std::unordered_map<std::string, std::function<void(int, HttpRequest)>> get_handlers;
+  std::unordered_map<std::string, std::function<void(int, HttpRequest)>> post_handlers;
+  void on_http(int client_fd);
 };
 
-
-#endif //WEBSOCKET___WEBSERVER_HPP
+#endif // WEBSOCKET___WEBSERVER_HPP
--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include "WebServer.hpp"
 
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
 
 int main() {
-    WebServer server;
-    server.listen(1234);
-    server.get("/", [](int client_fd, const HttpRequest &req) {
-        for (const auto &[key, val] : req.headers) {
-            std::cout << key << ": " << val << std::endl;
-        }
-        const char* response =
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/plain\r\n"
-                "Content-Length: 13\r\n"
-                "\r\n"
-                "hello!";
-        write(client_fd, response, std::strlen(response));
-    });
+  WebServer server{{"127.0.0.1", 8080}};
+  server.get("/", [](int client_fd, const HttpRequest &req) {
+    for (const auto &[key, val] : req.headers) {
+      std::cout << key << ": " << val << std::endl;
+    }
+    const char *response = "HTTP/1.1 200 OK\r\n"
+                           "Content-Type: text/plain\r\n"
+                           "Content-Length: 13\r\n"
+                           "\r\n"
+                           "hello!";
+    write(client_fd, response, std::strlen(response));
+  });
 
-    server.run();
-    return 0;
+  server.run();
+  server.wait_for_exit();
+  return 0;
 }
-
